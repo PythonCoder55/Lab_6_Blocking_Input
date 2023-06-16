aa16c171f014fbf6ad2e42d8690d529fe774fb57
PythonCoder55 <edwardbarchv@gmail.com>
2023-06-15 20:55:32 -0400

    task 2 done

---
+++ src/FuelCosts.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class FuelCosts {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double gallonsInTank;
+        double fuelEfficiency;
+        double pricePerGallon;
+
+        do {
+            System.out.println("Enter the number of gallons of gas in the tank(must be positive): ");
+            if(in.hasNextDouble())
+            {
+                gallonsInTank = in.nextDouble();
+            }
+            else {
+                System.out.println("You did not enter a double!");
+                in.nextLine();
+                gallonsInTank = -1;
+            }
+        } while (gallonsInTank <= 0);
+
+        do {
+            System.out.println("Enter the fuel efficiency in miles per gallon(must be positive): ");
+            if(in.hasNextDouble()) {
+                fuelEfficiency = in.nextDouble();
+            }
+            else {
+                System.out.println("You did not enter a double!");
+                in.nextLine();
+                fuelEfficiency = -1;
+            }
+        } while (fuelEfficiency <= 0);
+
+        do {
+            System.out.println("Enter the price of gas per gallon(must be positive): ");
+            if(in.hasNextDouble()) {
+                pricePerGallon = in.nextDouble();
+            }
+            else {
+                System.out.println("You did not enter a double!");
+                in.nextLine();
+                pricePerGallon = -1;
+            }
+        } while (pricePerGallon <= 0);
+
+        double cost = (100 / fuelEfficiency) * pricePerGallon;
+        double distance = gallonsInTank * fuelEfficiency;
+
+        System.out.println("Cost per 100 miles: $" + cost);
+        System.out.println("Distance the car can go with the gas in the tank: " + distance + " miles");
+    }
+}
